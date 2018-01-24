3f58879ce20b054c63d1797eeb5bf10284ab018e
Chris Turina and Michael Arrington <chris+michael@>
2018-01-23 16:53:50 -0800

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,54 @@
+// import React from 'react';
+// import { Route, Switch } from 'react-router-dom';
+// import Header from './Header';
+// import FeedPage from './FeedPage';
+// import NewPostForm from './NewPostForm';
+//
+//
+// class App extends React.Component{
+//
+//   constructor(props){
+//     super(props);
+//     this.state = {
+//       masterPostArray: [
+//         { title: 'Sup', description: 'Yo this is my favorite post. Hopefully you think so to and upvote it!', counter: 0 },
+//         { title: 'Hello?', description: 'Is this thing working?', counter: 1 },
+//         { title: 'Drrrrr', description: 'Notsogoodatypingwell!11', counter: 2 }
+//
+//       ]
+//
+//     };
+//     this.handleAddingPostToList = this.handleAddingPostToList.bind(this);
+//     this.handleSortList = this.handleSortList.bind(this);
+//   }
+//
+//   handleSortList(post){
+//
+//   }
+//
+//   handleAddingPostToList(newPost){
+//     let newMasterPostArray = this.state.masterPostArray.slice();
+//     newMasterPostArray.push(newPost);
+//     this.setState({masterPostArray: newMasterPostArray});
+//   }
+//
+//   render(){
+//     return(
+//       <div>
+//         <Header />
+//         <Switch>
+//           <Route exact path='/' render={()=> <FeedPage postFeed={this.state.masterPostArray} onVoteSort={this.handleSortList}/>} />
+//           <Route path='/newpostform' render={()=> <NewPostForm onNewPostCreation={this.handleAddingPostToList} />} />
+//         </Switch>
+//       </div>
+//     );
+//   }
+//
+// }
+//
+//
+// export default App;
+
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Header from './Header';
@@ -20,9 +71,17 @@ class App extends React.Component{
     };
     this.handleAddingPostToList = this.handleAddingPostToList.bind(this);
     this.handleSortList = this.handleSortList.bind(this);
+    this.handleVote = this.handleVote.bind(this);
   }
 
   handleSortList(post){
+    let newMasterSortArray = this.state.masterPostArray.slice();
+
+  }
+
+  handleVote(thing) {
+    console.log(thing)
+    update thing from master list...
 
   }
 
@@ -37,7 +96,10 @@ class App extends React.Component{
       <div>
         <Header />
         <Switch>
-          <Route exact path='/' render={()=> <FeedPage postFeed={this.state.masterPostArray} onVoteSort={this.handleSortList}/>} />
+          <Route exact path='/' render={()=> <FeedPage
+              postFeed={this.state.masterPostArray}
+              onVoteSort={this.handleVote}
+            />} />
           <Route path='/newpostform' render={()=> <NewPostForm onNewPostCreation={this.handleAddingPostToList} />} />
         </Switch>
       </div>
--- src/components/Voting.jsx
+++ src/components/Voting.jsx
@@ -1,24 +1,106 @@
+// import React from 'react';
+//
+// class Voting extends React.Component {
+//
+//   constructor(props) {
+//     super(props);
+//     this.state = {};
+//     this.onUpClick = this.onUpClick.bind(this);
+//     this.onDownClick = this.onDownClick.bind(this);
+//
+//   }
+//
+//
+//
+//   onUpClick(props) {
+//     // setState(this.props.counter);
+//     // _counter +=1;
+//     // // console.log(_counter);
+//     // // this.setState({counter: _counter + 1})
+//     // console.log(_counter);
+//
+//     this.setState({ this.props.counter += 1 });
+//     console.log();
+//     // props.onVoteSort({counter: _counter.value});
+//
+//   }
+//
+//   onDownClick(props) {
+//     this.setState({ counter: this.props.counter - 1 })
+//   }
+//
+//   render(props){
+//     console.log(this.props);
+//     const styles = {
+//       containerStyle: {
+//         display: 'flex',
+//         flexDirection: 'column',
+//         justifyContent: 'center',
+//         marginRight: '10px',
+//         width: '70px',
+//         alignItems: 'center'
+//       },
+//       arrowStyle: {
+//         width: '15px',
+//         height: '15px',
+//       },
+//       arrowStyleUpsideDown: {
+//         height: '15px',
+//         width: '15px',
+//         transform: 'rotate(180deg)'
+//       },
+//       counter: {
+//         fontSize: '24px',
+//         lineHeight: '10px',
+//         color: '#666'
+//       }
+//     };
+//
+//     return(
+//
+//       <div style={styles.containerStyle}>
+//         <img onClick={this.onUpClick} style={styles.arrowStyle} src='https://cdn.pixabay.com/photo/2013/07/13/09/50/arrow-156118_1280.png' />
+//         <p style={styles.counter}>{this.props.counter}</p>
+//         <img onClick={this.onDownClick} style={styles.arrowStyleUpsideDown} src='https://cdn.pixabay.com/photo/2013/07/13/09/50/arrow-156118_1280.png' />
+//       </div>
+//     );
+//   }
+// }
+//
+//
+// export default Voting;
+
 import React from 'react';
 
 class Voting extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {};
-    this.onUpClick = this.onUpClick.bind(this);
-    this.onDownClick = this.onDownClick.bind(this);
+    this.state = {
 
+    };
+
+  }
+
+  onVote(direction) {
+    if (direction === 'up') {
+      props.handleVote('up');
+    } else {
+      props.handleVote('down');
+
+    }
   }
 
 
   onUpClick(props) {
-    let _counter = null;
-    _counter = this.props.counter;
-    console.log(_counter);
-    this.setState({counter: _counter + 1})
-    console.log(_counter);
-    
-    // this.setState({ counter: this.props.counter + 1 })
+    // setState(this.props.counter);
+    // _counter +=1;
+    // // console.log(_counter);
+    // // this.setState({counter: _counter + 1})
+    // console.log(_counter);
+
+    this.setState({ this.props.counter += 1 });
+    console.log();
     // props.onVoteSort({counter: _counter.value});
 
   }
